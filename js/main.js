6920e875553c4710a8a465a3415df44695f3e661
daniel makarzec <danielmakarzec@gmail.com>
2020-10-07 23:36:47 +0100

    v0.1.0

--- js/main.js
+++ js/main.js
@@ -1,14 +1,10 @@
 //  variables
-const btn = document.getElementById('dropbtn');
+const hamburger = document.getElementById('dropbtn');
 const dropMenu = document.getElementById('dropMenu');
 const dropMenuOptions = document.querySelectorAll('.btn-option');
-const about = document.getElementById('about');
-const contact = document.getElementById('contact');
-const menu = document.getElementById('menu');
-const career = document.getElementById('career');
 
 // listeners
-btn.addEventListener('click', e => {
+hamburger.addEventListener('click', e => {
     e.preventDefault();
     dropMenu.classList.toggle('block');
 })
@@ -16,10 +12,9 @@ btn.addEventListener('click', e => {
 dropMenuOptions.forEach(btn => {
     btn.addEventListener('click', e => {
         e.preventDefault();
-        dropMenu.classList.toggle('block');
-        let target = e.target.attributes.href.value;
-        let section = document.getElementById(target.slice(1))
-        // console.log(target.slice(1));
+        let target = e.target.attributes.href.value.slice(1);
+        let section = document.getElementById(target)
         section.scrollIntoView();
+        dropMenu.classList.toggle('block');
     })
 })
